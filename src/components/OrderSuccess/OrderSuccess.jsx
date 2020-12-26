953c8c53eb6300383d049eb3b10571661e353578
kdingu <klaididingu@gmail.com>
2020-12-26 18:32:57 +0100

    done order success page

--- src/components/OrderSuccess/OrderSuccess.jsx
+++ src/components/OrderSuccess/OrderSuccess.jsx
@@ -1,16 +1,31 @@
+import { Grid, Paper, Typography } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
 import useStyles from "./styles";
+import imageUrl from "../../assests/crayon-1779.png";
 
 const OrderSuccess = () => {
   const classes = useStyles();
+
+  const orderRef = useSelector((state) => state.order);
+
   return (
     <div className={classes.main}>
-      <img
-        className={classes.background}
-        src="https://image.freepik.com/free-vector/hand-drawn-clapping-human-hands-doodle-set-collection-pencil-chalk-drawing-sketches-men-women-raising-arms-making-applause-isolated_160308-590.jpg"
-      />
-      <div className={classes.toolbar} />
-      <div>FALEMINDERIT!</div>
+      <img src={imageUrl} width={225} className={classes.img} />
+      <Paper className={classes.paper} elevation={3}>
+        <Typography variant="h4" align="center">
+          <span className={classes.orderRef}>{orderRef}</span>
+        </Typography>
+        <div className={classes.body}>
+          <Typography variant="p" component="p" className={classes.text}>
+            Ky është kodi i porosisë tuaj, ruajeni. Me këtë kod mund të
+            kontaktoni me suportin për çdo kërkesë.
+          </Typography>
+          <Typography variant="body2" className={classes.subtitle}>
+            Faleminderit
+          </Typography>
+        </div>
+      </Paper>
     </div>
   );
 };
--- src/components/OrderSuccess/styles.js
+++ src/components/OrderSuccess/styles.js
@@ -3,8 +3,42 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   main: {
-    border: "1px solid red",
-    width: "100%",
-    minHeight: "100vh",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+  },
+  paper: {
+    padding: "20px",
+    // width: "100%",
+    maxWidth: "450px",
+    display: "flex",
+    flexFlow: "column",
+    alignItems: "center",
+  },
+  orderRef: {
+    backgroundColor: theme.palette.secondary.dark,
+    paddingLeft: "13px",
+    paddingRight: "13px",
+    borderRadius: "20px",
+  },
+  body: {
+    border: "1px solid rgba(0,0,0,0.1)",
+    marginTop: "20px",
+    padding: "20px",
+    maxWidth: "300px",
+  },
+  img: {
+    zIndex: "-1",
+    position: "absolute",
+    top: "50%",
+    left: "100%",
+    transform: "translate(-50%,-50%)",
+  },
+  subtitle: {
+    fontSize: "25px",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: "20px",
   },
 }));
