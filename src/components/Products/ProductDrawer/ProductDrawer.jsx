4f6088d6269dc624c00e5f16d2046f4794a70fd9
kdingu <klaididingu@gmail.com>
2021-01-08 17:49:44 +0100

    rework on product drawer, still unfinished

--- src/components/Products/ProductDrawer/ProductDrawer.jsx
+++ src/components/Products/ProductDrawer/ProductDrawer.jsx
@@ -1,10 +1,12 @@
 import {
   Button,
   CircularProgress,
+  Divider,
   Drawer,
   FormControl,
   Grid,
   InputLabel,
+  MenuItem,
   Select,
   Typography,
 } from "@material-ui/core";
@@ -17,9 +19,8 @@ import {
   generalActions,
   productActions,
 } from "../../../store/actions";
-import { useForm } from "react-hook-form";
 import ImageCarousel from "./ImageCarousel/ImageCarousel";
-import RelatedProducts from "../Product/Product";
+import Product from "../Product/Product";
 
 const formatter = new Intl.NumberFormat("en-GB", {
   style: "currency",
@@ -29,19 +30,43 @@ const formatter = new Intl.NumberFormat("en-GB", {
 const ProductDrawer = () => {
   const classes = useStyles();
 
-  const { handleSubmit, register } = useForm();
-
   const dispatch = useDispatch();
   const product = useSelector((state) => state.products.productInDrawer);
   const open = useSelector((state) => state.products.productDrawer);
 
+  const [state, setState] = useState({});
   const [selectedOptionsPrice, setSelectedOptionsPrice] = useState(0);
   const [disabled, setDisabled] = useState(false);
   const [showSpinner, setShowSpinner] = useState(false);
 
   useEffect(() => {
     if (selectedOptionsPrice !== 0) setSelectedOptionsPrice(0);
+    setState({});
   }, [product]);
+
+  useEffect(() => {
+    const updatePrice = () => {
+      let total = 0;
+      // parse price for selected option(s)
+      const selectedVariants = Object.entries(state);
+      for (const selected of selectedVariants) {
+        const variant = product.variants.filter(
+          (variant) => variant.id === selected[0]
+        );
+        const option =
+          variant[0]?.options.filter((option) => option.id === selected[1]) ||
+          0;
+        total += option[0]?.price.raw || 0;
+      }
+      // update selectedOptionsPrice
+      console.log(total);
+      setSelectedOptionsPrice(total);
+    };
+
+    if (Object.keys(state).length !== 0) {
+      updatePrice();
+    }
+  }, [state]);
 
   const closeDrawer = () => {
     dispatch(productActions.closeDrawer());
@@ -56,76 +81,69 @@ const ProductDrawer = () => {
 
   const images = product.assets?.map((asset) => asset.url);
 
-  const addToCart = (data) => {
-    console.log(data);
-
-    setDisabled(true);
-    dispatch(cartActions.addProductToCartAsync(product.id, 1, data))
-      .then(() => setDisabled(false))
-      .catch((error) => {
-        console.log(error);
-        setDisabled(false);
-        dispatch(generalActions.changeErrorsCount());
-      });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
   };
 
-  const updatePrice = (data) => {
-    const transformed = Object.entries(data).map(([variant, option]) => ({
-      variant,
-      option,
-    }));
-
-    console.log(data);
-
-    let total = 0;
-    for (const selected of transformed) {
-      console.log(selected);
-      // if (!selected.option) continue;
-      // let ops = product.variants.filter(
-      //   (variant) => variant.id === selected.variant
-      // )[0].options;
-      // let selectedOption = ops.filter(
-      //   (option) => option.id === selected.option
-      // )[0];
-      // total += selectedOption.price.raw;
-    }
-
-    setSelectedOptionsPrice(total);
-  };
-
-  const primaryOptions = {
-    type: "loop",
-    perPage: 1,
-    perMove: 1,
-    gap: "1rem",
-    pagination: false,
-  };
-
-  const thumbnailOptions = {
-    type: "slide",
-    rewind: true,
-    gap: "1rem",
-    pagination: false,
-    fixedWidth: 110,
-    fixedHeight: 70,
-    cover: true,
-    focus: "center",
-    isNavigation: true,
-    updateOnMove: true,
-  };
+  const ProductVariants = ({ variant }) => (
+    <Grid item xs={12}>
+      <FormControl fullWidth required>
+        <InputLabel id={variant.id}>{variant.name}</InputLabel>
+        <Select
+          labelId={variant.id}
+          id={variant.id}
+          name={variant.id}
+          defaultValue={""}
+          value={state[variant.id]}
+          onChange={(e) => {
+            setState({ ...state, [e.target.name]: e.target.value });
+          }}
+        >
+          {variant.options.map((option) => (
+            <MenuItem
+              key={option.id}
+              value={option.id}
+              disabled={option.is.quantity_limited && option.quantity === 0}
+            >
+              {option.name}{" "}
+              {option.price.raw !== 0
+                ? `+${option.price.formatted_with_code}`
+                : null}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Grid>
+  );
 
   if (!product || !product.id) return null;
 
   return (
-    <>
-      <Drawer
-        className={classes.root}
-        anchor={"right"}
-        open={open}
-        onClose={closeDrawer}
+    <Drawer
+      className={classes.root}
+      anchor={"right"}
+      open={open}
+      onClose={closeDrawer}
+    >
+      <div
+        className={classes.main}
+        role="presentation"
+        style={{ maxHeight: "100vh" }}
       >
-        <div className={classes.main} role="presentation">
-          <Grid container spacing={5}>
+        {showSpinner ? (
+          <div className={classes.loader}>
+            <CircularProgress />
+          </div>
+        ) : null}
+        <form onSubmit={handleSubmit}>
+          <Grid
+            container
+            spacing={5}
+            style={{
+              maxHeight: "100%",
+            }}
+          >
             {/* product images gallery */}
             <Grid item xs={12} lg={6}>
               <ImageCarousel images={images} />
@@ -133,12 +151,84 @@ const ProductDrawer = () => {
 
             {/* product details */}
             <Grid container item xs={12} lg={6}>
-              details
+              <Grid item xs={12}>
+                <Typography variant="h2">{product.name}</Typography>
+                <Typography variant="h3">
+                  {formatter.format(product.price.raw + selectedOptionsPrice)}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Divider
+                  variant="middle"
+                  style={{ marginTop: "36px", marginBottom: "36px" }}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant="h5">Përshkrimi i produktit:</Typography>
+                <Typography variant="p">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: `${product.description}`,
+                    }}
+                  ></span>
+                </Typography>
+              </Grid>
+
+              {product.variants?.length ? (
+                <Grid item xs={12} style={{ marginTop: "36px" }}>
+                  <Typography variant="h5">
+                    Variantet e disponueshme:
+                  </Typography>
+                  <Grid container spacing={1}>
+                    {product.variants.map((variant) => (
+                      <ProductVariants variant={variant} />
+                    ))}
+                  </Grid>
+                </Grid>
+              ) : null}
+
+              <Grid item xs={12} align="right" style={{ marginTop: "36px" }}>
+                <Button
+                  size="large"
+                  variant="contained"
+                  disableElevation
+                  color="secondary"
+                  type="submit"
+                >
+                  Shto në shportë
+                </Button>
+              </Grid>
+
+              {product.related_products?.length ? (
+                <>
+                  <Grid item xs={12}>
+                    <Divider
+                      variant="middle"
+                      style={{ marginTop: "36px", marginBottom: "36px" }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="h5">Produkte të ngjashme:</Typography>
+                    <Grid container spacing={1}>
+                      {product.related_products.map((product) => (
+                        <Grid item xs={6} style={{ marginTop: "26px" }}>
+                          <Product
+                            product={product}
+                            handleClick={() => setProductInDrawer(product.id)}
+                          />
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                </>
+              ) : null}
             </Grid>
           </Grid>
-        </div>
-      </Drawer>
-    </>
+        </form>
+      </div>
+    </Drawer>
   );
 };
 
--- src/components/Products/ProductDrawer/styles.js
+++ src/components/Products/ProductDrawer/styles.js
@@ -5,4 +5,16 @@ export default makeStyles((theme) => ({
     width: "80vw",
     padding: 56,
   },
+  loader: {
+    zIndex: 1000,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(255,255,255,0.8)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 }));
