4f1d54857f5a7740520631476610c15346d874fe
kdingu <klaididingu@gmail.com>
2021-01-15 11:47:33 +0100

    made footer fixed bottom

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,21 +2,17 @@ import { makeStyles, fade } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   main: {
-    // color: theme.palette.primary.contrastText,
     color: "#0f0f0f",
+    position: "fixed",
+    bottom: 0,
     padding: "0",
     backgroundColor: theme.palette.primary.main,
     boxShadow: "0px -10px 15px rgba(0,0,0,0.15)",
-  },
-  content: {
-    // padding: "20px",
   },
   signature: {
     textAlign: "center",
     paddingTop: "10px",
     paddingBottom: "10px",
-    // borderTop: "1px solid #000000a0",
-    // backgroundColor: "#84DCF1",
     backgroundColor: theme.palette.background.light,
   },
   menu: {
