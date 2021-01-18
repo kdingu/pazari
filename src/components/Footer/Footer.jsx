c61aebc2db3bee6865d45138af250aa386d3a0f8
kdingu <klaididingu@gmail.com>
2021-01-18 16:25:08 +0100

    removed comment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,17 +9,6 @@ const Footer = () => {
 
   return pathname !== "/checkout" ? (
     <Container id="footer" maxWidth="xl" className={classes.main}>
-      {/* <Container className={classes.content}>
-        <Grid container className={classes.menu}>
-          {menuItems.map((item) => (
-            <Grid item align="center" xs={12} md={4} key={item.title}>
-              <Button component={Link} to={item.link} className={classes.item}>
-                {item.title}
-              </Button>
-            </Grid>
-          ))}
-        </Grid>
-      </Container> */}
       <div className={classes.signature}>
         <Container>© Pazari - {new Date().getFullYear()}</Container>
       </div>
